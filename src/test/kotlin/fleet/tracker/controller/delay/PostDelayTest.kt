bab7352be7346019e426bd9b86e35104ac9edd3b
naruto1031 <naruto0614@au.com>
2024-06-21 16:00:20 +0900

    feat: インデント修正&不要なアノテーションの削除

--- src/test/kotlin/fleet/tracker/controller/delay/PostDelayTest.kt
+++ src/test/kotlin/fleet/tracker/controller/delay/PostDelayTest.kt
@@ -8,18 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.transaction.annotation.Transactional
-import java.awt.PageAttributes
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@Sql("/warehouse/Insert_Warehouse_Test_Data.sql")
 class PostDelayTest {
 
     @Autowired
@@ -129,7 +126,7 @@ class PostDelayTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
                     {
-                    "invalid_param": "invalid_value"
+                      "invalid_param": "invalid_value"
                     }
                 """.trimIndent()))
 
